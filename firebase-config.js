4351a9e9a5ad0842cd6daa73d4ca58e263e7340b
anaferr02 <94547689+anaferr02@users.noreply.github.com>
2026-03-16 10:35:09 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "ACA_TU_API_KEY",
-  authDomain: "ACA_TU_AUTH_DOMAIN",
-  projectId: "ACA_TU_PROJECT_ID",
-  storageBucket: "ACA_TU_STORAGE_BUCKET",
-  messagingSenderId: "ACA_TU_MESSAGING_SENDER_ID",
-  appId: "ACA_TU_APP_ID"
+  apiKey: "AIzaSyCj82nKioR0zTYQJPMUme0yFCXDKivVC_g",
+  authDomain: "tualoja-5210e.firebaseapp.com",
+  projectId: "tualoja-5210e",
+  storageBucket: "tualoja-5210e.firebasestorage.app",
+  messagingSenderId: "472294290866",
+  appId: "1:472294290866:web:7bc5d811c4c63bea9660ab",
+  measurementId: "G-M9H49YEZSN"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
