68207e12e55c009fd64a299c3a84f7bf07b7605d
anaferr02 <94547689+anaferr02@users.noreply.github.com>
2026-04-17 18:32:12 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,7 +7,8 @@ const firebaseConfig = {
   apiKey: "AIzaSyCj82nKioR0zTYQJPMUme0yFCXDKivVC_g",
   authDomain: "tualoja-5210e.firebaseapp.com",
   projectId: "tualoja-5210e",
-  storageBucket: "tualoja-5210e.appspot.com",
+  // CAMBIÁ ESTA LÍNEA AQUÍ ABAJO 👇
+  storageBucket: "tualoja-5210e.firebasestorage.app", 
   messagingSenderId: "472294290866",
   appId: "1:472294290866:web:7bc5d811c4c63bea9660ab",
   measurementId: "G-M9H49YEZSN"
